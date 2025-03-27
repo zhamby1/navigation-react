79fbeeb932488b8c56c6d275f8822dacab0b50ad
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-03-27 10:59:00 -0400

    dsf

--- App.js
+++ App.js
@@ -1,41 +1,32 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Platform, Settings } from "react-native";
-import Home from "./Home"
+import { Platform } from "react-native";
+import Home from "./Home";
 import News from "./News";
-import Settings from "./Settings";
-
+import SettingsTab from "./Settings";
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-//add options to the Details screen to accept a route
-//header right which is a function that returns a button on the upper right hand part of the screen (upper portion is known as the header)
-//return a button with the text of Buy and create a route paramater called stock that equal 0
-
-//jjj
 export default function App() {
   return (
     <NavigationContainer>
       {Platform.OS === "ios" && (
         <Tab.Navigator>
-         <Tab.Screen name="Home" component={Home} />
-         <Tab.Screen name="News" component={News} />
-         <Tab.Screen name="Settings" component={Settings} />
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="News" component={News} />
+          <Tab.Screen name="Settings" component={SettingsTab} />
         </Tab.Navigator>
       )}
 
-      {Platform.OS === "android" && (
+      {Platform.OS == "android" && (
         <Drawer.Navigator>
-         <Drawer.Screen name="Home" component={Home} />
-         <Drawer.Screen name="News" component={News} />
-         <Drawer.Screen name="Settings" component={Settings} />
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="News" component={News} />
+          <Drawer.Screen name="Settings" component={SettingsTab} />
         </Drawer.Navigator>
       )}
-
-      
-
     </NavigationContainer>
   );
 }
--- Settings.js
+++ Settings.js
@@ -2,7 +2,7 @@ import React from "react";
 import {View, Text} from "react-native";
 import styles from "./styles";
 
-export default function Settings({navigation}){
+export default function SettingsTab({navigation}){
     return(
         <View style = {styles.container}>
           <Text>Settings Content</Text>
