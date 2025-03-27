eae614fee6293792936789aa0b33878b76ce6fa3
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-03-27 11:05:00 -0400

    headersnav

--- App.js
+++ App.js
@@ -1,41 +1,32 @@
+import { Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Platform, Settings } from "react-native";
-import Home from "./Home"
-import News from "./News";
-import Settings from "./Settings";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./Home";
+import Details from "./Details";
 
-
-const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
-
-//add options to the Details screen to accept a route
-//header right which is a function that returns a button on the upper right hand part of the screen (upper portion is known as the header)
-//return a button with the text of Buy and create a route paramater called stock that equal 0
-
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {Platform.OS === "ios" && (
-        <Tab.Navigator>
-         <Tab.Screen name="Home" component={Home} />
-         <Tab.Screen name="News" component={News} />
-         <Tab.Screen name="Settings" component={Settings} />
-        </Tab.Navigator>
-      )}
-
-      {Platform.OS === "android" && (
-        <Drawer.Navigator>
-         <Drawer.Screen name="Home" component={Home} />
-         <Drawer.Screen name="News" component={News} />
-         <Drawer.Screen name="Settings" component={Settings} />
-        </Drawer.Navigator>
-      )}
-
-      
-
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={({ route }) => ({
+            headerRight: () => {
+              return (
+                <Button
+                  title="Buy"
+                  onPress={() => {}}
+                  disabled={route.params.stock === 0}
+                />
+              );
+            },
+          })}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
