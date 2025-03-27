d87474fa526958c5a5ae376e7b5e8dc3ceef739b
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-03-27 10:48:26 -0400

    tabdrawernav

--- News.js
+++ News.js
@@ -2,10 +2,10 @@ import React from "react";
 import {View, Text} from "react-native";
 import styles from "./styles";
 
-export default function Home({navigation}){
+export default function News({navigation}){
     return(
         <View style = {styles.container}>
-          <Text>Home Content</Text>
+          <Text>News Content</Text>
         </View>
     )
 }
--- Settings.js
+++ Settings.js
@@ -2,10 +2,10 @@ import React from "react";
 import {View, Text} from "react-native";
 import styles from "./styles";
 
-export default function Home({navigation}){
+export default function Settings({navigation}){
     return(
         <View style = {styles.container}>
-          <Text>Home Content</Text>
+          <Text>Settings Content</Text>
         </View>
     )
 }
